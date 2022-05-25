8bde1fdd56c801b90ce14085fbd5b72cf8e04e0c
mahiro06 <shishamo06@gmail.com>
2022-05-25 23:47:50 +0900

    商品出品機能実装

---
+++ app/models/item.rb
@@ -0,0 +1,22 @@
+class Item < ApplicationRecord
+  extend ActiveHash::Associations::ActiveRecordExtensions
+  belongs_to :category
+  belongs_to :quality
+  belongs_to :prefecture
+  belongs_to :shipping_date
+  belongs_to :shipping_fee
+
+  belongs_to :user
+  has_one_attached :image
+
+
+  validates :item_name, :item_info, :category_id, :quality_id, :prefecture_id, :shipping_date_id, :shipping_fee_id, :price, :image, presence: true
+  validates :category_id, :quality_id, :prefecture_id, :shipping_date_id, :shipping_fee_id, numericality: { other_than: 1, message: "can't be blank" }
+  with_options allow_blank: true, format: { with: /\A[0-9]+\z/, message: '半角数字を使用してください' } do
+    validates :price, numericality: {only_integer: true, greater_than_or_equal_to: 300, less_than_or_equal_to:9999999}
+  end
+  validates :item_name, length: {maximum: 40}
+  validates :item_info, length: {maximum: 1000}
+
+
+end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,8 +1,11 @@
 class User < ApplicationRecord
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable, :trackable and :omniauthable
+  
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :validatable
+
+  has_many :items
 
   validates :nickname, :last_name, :first_name, :last_name_kana, :first_name_kana, :birth, presence: true
   with_options allow_blank: true, format: { with: /\A[ぁ-んァ-ヶ一-龥々ー]+\z/, message: '全角文字を使用してください' } do
