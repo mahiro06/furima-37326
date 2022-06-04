d8754889391315f5afee1f1f1c69364a5520c93e
mahiro06 <shishamo06@gmail.com>
2022-06-04 15:33:32 +0900

    migrateファイル修正実施

--- db/migrate/20220530054757_create_addresses.rb
+++ db/migrate/20220530054757_create_addresses.rb
@@ -1,7 +1,7 @@
 class CreateAddresses < ActiveRecord::Migration[6.0]
   def change
     create_table :addresses do |t|
-      t.string      :postal_cord,        null: false
+      t.string      :postal_code,        null: false
       t.integer     :prefecture_id,      null: false
       t.string      :city,               null: false
       t.string      :building_address,  null: false
