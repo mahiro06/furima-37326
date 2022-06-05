fc475a1ab42ec9639442df55af9b3a15366fee0b
mahiro06 <shishamo06@gmail.com>
2022-06-05 10:51:10 +0900

    修正

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -53,6 +53,6 @@ class ItemsController < ApplicationController
 
   def move_to_index
     redirect_to action: :index unless current_user.id == @item.user_id
-    redirect_to action: :index unless Rop.find_by(item_id: params[:item_id])
+    redirect_to action: :index unless Rop.find_by(item_id: params[:item_id]).nil?
   end
 end
